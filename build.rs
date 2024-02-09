8ebf7ee9e8c80590eae6c809d25b628e91c96642
NightMare-Vortex <mirza123.the.best@gmail.com>
2024-02-09 22:23:09 +0300

    0.1.3 fixed build script

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
-use std::fs::File;
-use std::io::Write;
-use proc_macro2::TokenStream;
-
 fn main() {
-    let src = include_str!("./src/minified.rs").parse::<TokenStream>()
-        .unwrap();
-    write!(File::create("./src/__minified.rs").unwrap(),
-    "#[cfg(not(feature = \"local-build\"))] #[macro_use] pub mod online_judge_utils {{ {src} }}"
-    ).unwrap()
+//     use std::fs::File;
+//     use std::io::Write;
+//     use proc_macro2::TokenStream;
+//
+//     let src = include_str!("./src/minified.rs").parse::<TokenStream>()
+//         .unwrap();
+//     write!(File::create("./src/__minified.rs").unwrap(),
+//     "#[cfg(not(feature = \"local-build\"))] #[macro_use] pub mod online_judge_utils {{ {src} }}"
+//     ).unwrap()
 }
