71175eb385b8f544979dfd2c773a3d0e02fc7feb
Vrtgs <mirza123.the.best@gmail.com>
2024-10-17 22:50:19 +0300

    better build.rs

--- build.rs
+++ build.rs
@@ -1,34 +1,36 @@
-fn main() {
-    use proc_macro2::TokenStream;
-    use std::fs::File;
-    use std::io::Write;
+use proc_macro2::TokenStream;
+use std::fs::File;
+use std::io::Write;
 
-    let src = include_str!("./src/lib.rs")
-        .replace(
-            "pub mod rng;",
-            concat!("pub mod rng { ", include_str!("./src/rng.rs"), " }"),
-        )
-        .replace(
-            "pub mod modding_num;",
-            concat!(
+
+fn main() {
+    if std::fs::exists("./minified").unwrap() {
+        let src = include_str!("./src/lib.rs")
+            .replace(
+                "pub mod rng;",
+                concat!("pub mod rng { ", include_str!("./src/rng.rs"), " }"),
+            )
+            .replace(
+                "pub mod modding_num;",
+                concat!(
                 "pub mod modding_num { ",
                 include_str!("./src/modding_num.rs"),
                 " }"
-            ),
-        )
-        .parse::<TokenStream>()
-        .unwrap();
+                ),
+            )
+            .parse::<TokenStream>()
+            .unwrap();
 
-    let mini = format!("#[rustfmt::skip] #[cfg(not(feature = \"local-build\"))] #[macro_use] pub mod online_judge_utils {{ {src} }}")
-        .replace("$ crate", "online_judge_utils")
-        .replace("online_judge_utils :: read", "read")
-        .replace("online_judge_utils :: file_io", "file_io")
-        .replace("online_judge_utils :: output", "output")
-        .replace("online_judge_utils :: parse", "parse");
+        let mini = format!("#[rustfmt::skip] #[cfg(not(feature = \"local-build\"))] #[macro_use] pub mod online_judge_utils {{ {src} }}")
+            .replace("$ crate", "online_judge_utils")
+            .replace("online_judge_utils :: read", "read")
+            .replace("online_judge_utils :: file_io", "file_io")
+            .replace("online_judge_utils :: output", "output")
+            .replace("online_judge_utils :: parse", "parse");
 
-    std::fs::create_dir_all("./minified").unwrap();
-    File::create("./minified/__minified.rs")
-        .unwrap()
-        .write_all((mini + "\n").as_bytes())
-        .unwrap()
+        File::create("./minified/__minified.rs")
+            .unwrap()
+            .write_all((mini + "\n").as_bytes())
+            .unwrap()
+    }
 }
